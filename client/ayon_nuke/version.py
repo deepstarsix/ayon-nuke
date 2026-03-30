5665dae4b59dd15d7ba19953b1837ccb7eeaca71
Vicko <59090367+deepstarsix@users.noreply.github.com>
2026-03-30 07:48:59 +0200

    ver

--- client/ayon_nuke/version.py
+++ client/ayon_nuke/version.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'nuke' version."""
+<<<<<<< Updated upstream
 __version__ = "0.4.0+dev"
+=======
+__version__ = "0.4.1+poster"
+>>>>>>> Stashed changes
